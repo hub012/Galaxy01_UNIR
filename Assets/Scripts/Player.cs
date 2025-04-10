5e13fc05f24839bc2d52a34686e59f5db7a88d4f
hub012 <hub0121@gmail.com>
2025-04-10 12:39:06 +0200

    changed movement logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float speed = 5.0f;
      void Start()
     {
         
@@ -13,25 +15,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.right * (horizontal * (speed * Time.deltaTime)));
+        transform.Translate(Vector2.up * (vertical * (speed * Time.deltaTime)));
+            
+       /* if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-           transform.Translate(new Vector2(0,1));
+           //transform.Translate(new Vector2(0,1));
+           transform.Translate(Vector2.up * (speed * Time.deltaTime));
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector2(0,-1));
+            //transform.Translate(new Vector2(0,-1));
+            transform.Translate(Vector2.down * (speed * Time.deltaTime));
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.Translate(new Vector2(-1, 0));
+            //transform.Translate(new Vector2(-1, 0));
+            transform.Translate(Vector2.left * (speed * Time.deltaTime));
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.Translate(new Vector2(1, 0));
-        }
+            //transform.Translate(new Vector2(1, 0));
+            transform.Translate(Vector2.right * (speed * Time.deltaTime));
+        }*/
     }
 }
